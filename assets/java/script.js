8ae9a479ee5d15faff8d9f6404e08900904d2f02
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-05-05 21:15:40 -0700

    commit raw plan

---
+++ assets/java/script.js
@@ -0,0 +1,9 @@
+  //need api key
+  //set variables
+  //function will be call API for city by name
+  //function will be call APi for 5 day
+  //save results into local storage
+  //recall storage and display on click
+  // clear storage?
+
+  
